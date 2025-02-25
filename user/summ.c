01c71b7bf42bc52aa5881842c615019a22d86661
sh4rrkyyyy <fedotovdanil002@gmail.com>
2025-02-25 22:41:26 +0300

    fix

--- user/summ.c
+++ user/summ.c
@@ -15,30 +15,29 @@ int main(int argc, char *argv[]) {
       exit(1);
     }
     int cc = read(0, &c, 1);
-    if (c == ' '){
-        if (flag == 0x0){
-            pos = i;
-            flag = 0x1;
-        }
-        else {
-            write(2, "Incorrect format: More than one space\n", 38);
-            exit(1);
-        }
-    }
-    if (cc != 1) {
+    if (cc < 1) {
       write(2, "Read fail\n", 10);
       exit(1);
+    }
+    if (c == ' ') {
+      if (flag == 0x0) {
+        pos = i;
+        flag = 0x1;
+      } else {
+        write(2, "Incorrect format: More than one space\n", 38);
+        exit(1);
+      }
     }
     buf[i++] = c;
     if (c == '\n' || c == '\r')
       break;
   }
   buf[i - 1] = '\0';
-  printf("|%s|\n", buf);
   if (pos == -1) {
     write(2, "Incorrect format: no space\n", 27);
     exit(1);
   }
+  printf("|%s|\n", buf);
   buf[pos] = '\0';
   int num1 = atoi(buf);
   int num2 = atoi(buf + pos + 1);
