de6a720bf2871b4babcd09f1a227fa126d2a69b6
sh4rrkyyyy <fedotovdanil002@gmail.com>
2025-03-17 12:27:08 +0300

    fix

--- kernel/procinfo.h
+++ kernel/procinfo.h
@@ -1,4 +1,3 @@
-#pragma once
 
 struct procinfo {
   int pid;
--- user/ps.c
+++ user/ps.c
@@ -1,11 +1,32 @@
 #include "kernel/types.h"
 #include "user/user.h"
 #include "kernel/procinfo.h"
+#define MAXX 8
+
 int main(int argc, char* argv[]) {
-    struct procinfo pi[64];
-    int cnt = ps_listinfo(pi, 64);
-    printf("pid | name | state | parent_pid | parent_name\n");
-    for (int i = 0; i < cnt; ++i) {
-        printf("%d %s %s %d %s\n", pi[i].pid, pi[i].name, pi[i].state, pi[i].parent_pid, pi[i].parent_name);
+    int sz = 1;
+    int cnt = 0;
+    struct procinfo *pi = (struct procinfo *)malloc(sizeof(struct procinfo) * sz);
+    while (cnt < MAXX) {
+      cnt++;
+      int ret = ps_listinfo(pi, sz);
+      if (ret == -2) {
+        free(pi);
+        sz *= 2;
+        pi = (struct procinfo *)malloc(sizeof(struct procinfo) * sz);
+      }
+      else if (ret == -1) {
+        free(pi);
+        fprintf(2, "Error: cannot write to incorrect address\n");
+        exit(1);
+      }
+      else {
+        printf("pid | name | state | parent_pid | parent_name\n");
+        for (int i = 0; i < ret; ++i) {
+            printf("%d %s %s %d %s\n", pi[i].pid, pi[i].name, pi[i].state, pi[i].parent_pid, pi[i].parent_name);
+        } 
+        free(pi);
+        break;
+      }
     }
 }
