2aff097793e1c43239ed0280aaae0a6dbf16fd7c
sh4rrkyyyy <fedotovdanil002@gmail.com>
2025-03-25 22:29:27 +0300

    fix

--- user/mutex2.c
+++ user/mutex2.c
@@ -2,7 +2,7 @@
 #include "user/user.h"
 
 void proc_print(int argc, char *argv[], int mutex) {
-  for (int i = 0; i < argc; ++i) {
+  for (int i = 1; i < argc; ++i) {
     for (int j = 0; j < strlen(argv[i]); ++j) {
       mutex_lock(mutex);
       printf("pid: %d, arg: %d, char: %s\n", getpid(), i, (char[]){argv[i][j], 0});
@@ -11,6 +11,10 @@ void proc_print(int argc, char *argv[], int mutex) {
   }
 }
 int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(2, "Too few arguments\n");
+    exit(1);
+  }
   int mutex = mutex_create();
   int pid = fork();
   if (pid < 0) {
