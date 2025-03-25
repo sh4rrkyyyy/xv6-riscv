0077b634fe345b5598891801914b0962c7861f17
sh4rrkyyyy <fedotovdanil002@gmail.com>
2025-03-25 22:36:57 +0300

    fix

--- user/mutex2.c
+++ user/mutex2.c
@@ -10,13 +10,36 @@ void proc_print(int argc, char *argv[], int mutex) {
     }
   }
 }
+void proc_print_nosync(int argc, char *argv[]) {
+  for (int i = 1; i < argc; ++i) {
+    for (int j = 0; j < strlen(argv[i]); ++j) {
+      printf("pid: %d, arg: %d, char: %s\n", getpid(), i, (char[]){argv[i][j], 0});
+    }
+  }
+}
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(2, "Too few arguments\n");
     exit(1);
   }
-  int mutex = mutex_create();
+  printf("PRINT WITHOUT MUTEX\n");
   int pid = fork();
+  if (pid < 0) {
+    fprintf(2, "Fork error\n");
+  }
+  if (pid == 0) {
+    proc_print_nosync(argc, argv);
+    exit(0);
+  }
+  else {
+    proc_print_nosync(argc, argv);
+    wait(0);
+  }
+  printf("\n");
+  sleep(10);
+  printf("PRINT WITH MUTEX\n"); 
+  int mutex = mutex_create();
+  pid = fork();
   if (pid < 0) {
     fprintf(2, "Fork error\n");
   }
@@ -28,5 +51,6 @@ int main(int argc, char *argv[]) {
     proc_print(argc, argv, mutex);
     wait(0);
   }
+  mutex_close(mutex);
   
 }
