1f11b16495efb51e8f1ef7c40fb8a9d2ae94e48f
sh4rrkyyyy <fedotovdanil002@gmail.com>
2025-05-04 19:24:43 +0300

    fix

--- kernel/dev.c
+++ kernel/dev.c
@@ -1,4 +1,3 @@
-
 #include "types.h"
 #include "riscv.h"
 #include "param.h"
@@ -12,14 +11,17 @@
 #define MZERO 1
 #define MURANDOM 2
 #define MNULLSTAT 3
+#define BUFSIZE 256
 
 static struct {
   uint64 seed;
-  struct spinlock lock;
+  struct spinlock urandom_lock;
+  struct spinlock nullstat_lock;
   uint64 cnt_bytes;
 } dev = {.seed = 1};
 
 uint64 gen(uint64 seed) { return 214013 * seed + 2531011; }
+
 int devread(int minor, int user_dst, uint64 dst, int n) {
   if (n < 0) {
     return -1;
@@ -27,34 +29,38 @@ int devread(int minor, int user_dst, uint64 dst, int n) {
   if (minor == MNULL) {
     return 0;
   } else if (minor == MZERO) {
-    for (int i = 0; i < n; ++i) {
-      char smb = 0;
-      if (either_copyout(user_dst, dst + i, &smb, 1) < 0) {
+    char buf[BUFSIZE];
+    memset(buf, 0, BUFSIZE);
+    int cpy_cnt = 0;
+    while (cpy_cnt < n) {
+      int cnt = (n - cpy_cnt > BUFSIZE ? BUFSIZE : (n - cpy_cnt));
+      if (either_copyout(user_dst, dst + cpy_cnt, buf, cnt) < 0) {
         return -1;
       }
+      cpy_cnt += cnt;
     }
     return n;
   } else if (minor == MURANDOM) {
-    acquire(&dev.lock);
+    acquire(&dev.urandom_lock);
     uint64 seed = dev.seed;
     for (int i = 0; i < n; ++i) {
       seed = gen(seed);
       char byte = seed >> 56;
       if (either_copyout(user_dst, dst + i, &byte, 1) < 0) {
-        release(&dev.lock);
+        release(&dev.urandom_lock);
         return -1;
       }
     }
     dev.seed = seed;
-    release(&dev.lock);
+    release(&dev.urandom_lock);
     return n;
   } else if (minor == MNULLSTAT) {
     if (n != sizeof(uint64)) {
       return -1;
     }
-    acquire(&dev.lock);
+    acquire(&dev.nullstat_lock);
     uint64 cnt_bytes = dev.cnt_bytes;
-    release(&dev.lock);
+    release(&dev.nullstat_lock);
     if (either_copyout(user_dst, dst, &cnt_bytes, n) < 0) {
       return -1;
     }
@@ -80,21 +86,22 @@ int devwrite(int minor, int user_src, uint64 src, int n) {
     if (either_copyin(&seed, user_src, src, n) < 0) {
       return -1;
     }
-    acquire(&dev.lock);
+    acquire(&dev.urandom_lock);
     dev.seed = seed ? seed : 1;
-    release(&dev.lock);
+    release(&dev.urandom_lock);
     return n;
   } else if (minor == MNULLSTAT) {
-    acquire(&dev.lock);
+    acquire(&dev.nullstat_lock);
     dev.cnt_bytes += n;
-    release(&dev.lock);
+    release(&dev.nullstat_lock);
     return n;
   }
   return -1;
 }
 
 void devinit(void) {
-  initlock(&dev.lock, "dev");
+  initlock(&dev.urandom_lock, "dev_urandom");
+  initlock(&dev.nullstat_lock, "dev_nullstat");
   devsw[DEV].read = devread;
   devsw[DEV].write = devwrite;
 }
--- user/hexprint.c
+++ user/hexprint.c
@@ -4,7 +4,6 @@
 
 static char digits[] = "0123456789abcdef";
 
-#define BYTES 16
 void print_byte(unsigned char byte) {
   char s[3];
   s[0] = digits[byte >> 4];
@@ -14,7 +13,7 @@ void print_byte(unsigned char byte) {
 }
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
+  if (argc != 3) {
     fprintf(2, "hexprint: more or less than 2 arguments\n");
     exit(1);
   }
@@ -23,10 +22,19 @@ int main(int argc, char *argv[]) {
     fprintf(2, "hexprint: open failed\n");
     exit(1);
   }
-  unsigned char buf[BYTES];
-  int n = read(fd, buf, sizeof(buf));
+  int bytes = atoi(argv[2]);
+  unsigned char *buf = (unsigned char *)malloc(bytes);
+  if (buf == 0) {
+    fprintf(2, "hexprint: malloc failed\n");
+    close(fd);
+    exit(1);
+  }
+  int n = read(fd, buf, bytes);
   for (int i = 0; i < n; i++)
     print_byte(buf[i]);
+  
   printf("\n");
+  free(buf);
+  close(fd);
   exit(0);
 }
